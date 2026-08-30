6823cf44cdbc8ef199c5cda9b99af8d943355536
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-08-30 10:20:21 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  botName: process.env.BOT_NAME || "BONY-XMD",
-  ownerNumber: process.env.OWNER_NUMBER || "",
-  prefix: process.env.PREFIX || "."
-};
-
-module.exports = config;
