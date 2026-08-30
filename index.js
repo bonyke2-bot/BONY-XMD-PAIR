328abcf9dcb014c4de8583fa29b304a5991449a6
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-08-30 08:31:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/code', async (req, res) => {
     let sock = makeWASocket({
       auth: { creds: state.creds, keys: makeCacheableSignalKeyStore(state.keys, pino({level:"fatal"})) },
       logger: pino({level:"fatal"}),
-      browser: Browsers.Ubuntu("Chrome"),
+      browser: Browsers.ubuntu("Chrome"),
       printQRInTerminal: false
     });
     sock.ev.on("creds.update", saveCreds);
