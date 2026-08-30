7a22109e47fb884bcadf7b1256245fab48f988c5
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-08-30 08:42:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const pino = require("pino");
-let { toBuffer } = require("qrcode");
-const path = require('path');
 const fs = require("fs");
 const { default: makeWASocket, useMultiFileAuthState, delay, Browsers, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys");
 
@@ -12,68 +10,49 @@ function removeFile(FilePath) {
 };
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pair.html'));
+  res.send(`
+  <html>
+  <body style="background:black;color:white;text-align:center;padding:50px;font-family:sans-serif">
+  <h1>BONY-XMD PAIR</h1>
+  <p>Enter number with country code (2547...)</p>
+  <form action="/code" method="get">
+  <input name="number" placeholder="2547xxxxxxxx" style="padding:10px;width:200px"/>
+  <button style="padding:10px;background:green;color:white">GET CODE</button>
+  </form>
+  </body></html>
+  `);
 });
 
-// Pair Code Route
 app.get('/code', async (req, res) => {
-    let num = req.query.number;
-    if (!num) return res.status(400).send({ error: "Number required" });
+    let num = req.query.number?.replace(/[^0-9]/g,'');
+    if(!num) return res.send({error:"Add number ?number=2547..."});
     
-    num = num.replace(/[^0-9]/g, '');
-    
-    async function getPCode() {
-        const { state, saveCreds } = await useMultiFileAuthState(`./auth_${num}`);
-        try {
-            let sock = makeWASocket({
-                version: [2, 3000, 1023223821],
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-                },
-                printQRInTerminal: false,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: Browsers.ubuntu("Chrome"),
-                markOnlineOnConnect: false,
-            });
-
-            sock.ev.on('creds.update', saveCreds);
-
-            if (!sock.authState.creds.registered) {
-                await delay(3000);
-                let code = await sock.requestPairingCode(num);
-                console.log(`Pair code for ${num}: ${code}`);
-                if (!res.headersSent) {
-                    res.send({ code: code });
-                }
-                await delay(8000);
-                removeFile(`./auth_${num}`);
-            }
-
-        } catch (err) {
-            console.error("Error in pairing:", err);
+    const { state, saveCreds } = await useMultiFileAuthState(`./auth_${num}`);
+    try {
+        let sock = makeWASocket({
+            version: [2, 3000, 1023223821],
+            auth: {
+                creds: state.creds,
+                keys: makeCacheableSignalKeyStore(state.keys, pino({level:"fatal"}).child({level:"fatal"})),
+            },
+            logger: pino({level:"fatal"}).child({level:"fatal"}),
+            browser: Browsers.ubuntu("Chrome"),
+        });
+        sock.ev.on('creds.update', saveCreds);
+        if(!sock.authState.creds.registered){
+            await delay(3000);
+            let code = await sock.requestPairingCode(num);
+            if(!res.headersSent) res.send({code:code, number:num});
+            await delay(10000);
             removeFile(`./auth_${num}`);
-            if (!res.headersSent) {
-                res.status(503).send({ error: err.message });
-            }
         }
+    } catch(e){
+        removeFile(`./auth_${num}`);
+        if(!res.headersSent) res.status(503).send({error:e.message});
     }
-    await getPCode();
-});
-
-app.get('/pair', async (req, res) => {
-    let num = req.query.number;
-    if (!num) return res.status(400).send({ error: "Number required" });
-    num = num.replace(/[^0-9]/g, '');
-    res.redirect(`/code?number=${num}`);
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`BONY-XMD PAIR running on port ${PORT}`);
-});
-
-module.exports = app;
+app.listen(PORT, ()=> console.log("Running on "+PORT));
